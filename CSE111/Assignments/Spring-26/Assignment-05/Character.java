746861c94d04b54ebd8963b46523b92e920ec063
Estiuk uz zaman <mdestiukuzzaman@gmail.com>
2026-04-21 09:48:25 +0600

    A05_HT02

--- CSE111/Assignments/Spring-26/Assignment-05/Character.java
+++ CSE111/Assignments/Spring-26/Assignment-05/Character.java
@@ -1,3 +1,64 @@
 public class Character {
-    
+    public static String Strongest;
+    public static int Strongest_health;
+    public static int kids,teens,adults,count;
+    public static double sum;
+    public int id,health;
+    public String name,group;
+
+    public Character(String name,String group,int health){
+        this.name=name;
+        this.group=group;
+        this.health=health;
+        count++;
+        id=count;
+        if (health>Strongest_health) {
+            Strongest_health=health;
+            Strongest=name;
+        }
+        sum+=health;
+        if (group.equalsIgnoreCase("Kid")) {
+            this.kids++;
+        }
+        else if (group.equalsIgnoreCase("Adult")) {
+            this.adults++;
+        }
+        else{
+            this.teens++;
+        }
+    }
+    public Character(String name,int health){
+        this.name=name;
+        this.group="Teen";
+        this.health=health;
+        count++;
+        id=count;
+        if (health>Strongest_health) {
+            Strongest_health=health;
+            Strongest=name;
+        }
+        sum+=health;
+        teens++;
+    }
+
+    public static void printStats(){
+        System.out.println("Total Characters: "+count);
+        System.out.println("Kids: "+kids);
+        System.out.println("Teens: "+teens);
+        System.out.println("Adults: "+adults);
+        if (Strongest==null) {
+            System.out.println("Average Health: 0");
+            System.out.println("Strongest Character: None");
+        }
+        else{
+            System.out.println("Average Health: "+(sum/count));
+        System.out.println("Strongest Character: "+Strongest+" (Health "+Strongest_health+")");
+        }
+    }
+
+    public void printDetails(){
+        System.out.println("ID: "+id+", Name: "+name);
+        System.out.println("Group: "+group);
+        System.out.println("Health: "+health);
+    }
 }
