68a7c64fcac5c13cc41bfbec1667538059f029c6
estiuk <mdestiukuzzaman@gmail.com>
2026-05-22 15:09:57 -0700

    220_Ch0

---
+++ CSE220/LectureTest/FindTarget.java
@@ -0,0 +1,33 @@
+public class FindTarget {
+
+    public static void main(String[] args) {
+        int [] arr = {11,17,18,45,50,71,95};
+        int target = 50;
+
+        // for (int i =0;i<arr.length;i++){
+        //     if (arr[i]==target) {
+        //         System.out.println("Index: "+i);
+        //     }
+        // }    runtime=86ms
+
+        int m =0;
+        int n = arr.length-1;
+        while (m<=n) {
+            int l = (m+n)/2;
+            if (arr[l]==target) {
+                System.out.println(l);
+                break;
+            }
+            else{
+                if (arr[l]>target) {
+                    n=l-1;
+                }
+                else{
+                    m=l+1;
+                }
+            }
+        }
+
+        //runtime = 85ms (lol)
+    }
+}
